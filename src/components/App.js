24baf51e619bc395ad373ff749d1bc4113685ec0
Fabricio Brito <fabricio.brito@gmail.com>
2020-08-12 15:45:55 -0300

    feat: HandleInitialData call from  App Component
    Uses connect from redux to connect to store.
    Gets dispatch function from props and calls handleInitialData.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  };
+
   render() {
     return (
       <div>
         Starter Code
       </div>
     )
-  }
+  };
 }
 
-export default App
+// Connects the App to the store
+export default connect()(App)

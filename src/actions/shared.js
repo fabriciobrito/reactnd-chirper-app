1a36ef38e5fcc020edcfec48fd58400fb60443b6
Fabricio Brito <fabricio.brito@gmail.com>
2020-08-13 01:12:34 -0300

    feat: Add LoadingBar to App
    Import React-Redux-Loading to render loading bar while fetching data.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,7 @@ import { getInitialData } from '../utils/api';
 import { receiveTweets } from './tweets';
 import { receiveUsers } from './users';
 import { setAuthedUser } from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 // Mocking a authenticated user for the application
 const AUTHED_ID = 'tylermcginnis';
@@ -15,11 +16,13 @@ const AUTHED_ID = 'tylermcginnis';
  */
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(showLoading());
     return getInitialData()
       .then(( { users, tweets }) => {
         dispatch(receiveTweets(tweets));
         dispatch(receiveUsers(users));
         dispatch(setAuthedUser(AUTHED_ID));
+        dispatch(hideLoading());
       })
   }
 }
